2a9178b9fe45558de4f0485f23e9a0cc129397cd
Nguyen Minh Tuan <nguyenmtuan16042005@gmail.com>
2026-05-06 13:36:32 +0700

    fix pip command in script

--- scripts/collect_session01_evidence.ps1
+++ scripts/collect_session01_evidence.ps1
@@ -10,7 +10,7 @@ docker compose version 2>&1 | Out-File $toolFile -Append
 node --version 2>&1 | Out-File $toolFile -Append
 npm --version 2>&1 | Out-File $toolFile -Append
 python --version 2>&1 | Out-File $toolFile -Append
-pip --version 2>&1 | Out-File $toolFile -Append
+py -m pip --version 2>&1 | Out-File $toolFile -Append
 
 docker --version 2>&1 | Out-File "evidence/buoi-01/docker-version.txt"
 docker compose version 2>&1 | Out-File "evidence/buoi-01/compose-version.txt"
